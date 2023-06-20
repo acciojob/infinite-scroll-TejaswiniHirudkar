21b7c390dd787ed177eb83a2f858faf99c226ac8
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-06-20 19:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
 
+// Get the list element
+const list = document.getElementById('infi-list');
+
+// Add event listener to detect scrolling
+list.addEventListener('scroll', function() {
+  // Check if the user has reached the end of the list
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    // User has reached the end, add more list items
+    addMoreItems();
+  }
+});
+
+// Function to add more list items
+function addMoreItems() {
+  // Number of new items to add
+  const numItemsToAdd = 2;
+
+  // Generate new list items and append them to the list
+  for (let i = 1; i <= numItemsToAdd; i++) {
+    const newItem = document.createElement('li');
+    newItem.textContent = 'List Item ' + (list.childElementCount + i);
+    list.appendChild(newItem);
+  }
+}
